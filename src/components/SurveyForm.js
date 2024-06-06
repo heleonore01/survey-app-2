877f3d933c28f3bb83d992075dfeeceb646dd11c
heleonore01 <mueller.helena9@outlook.de>
2024-06-06 11:03:49 +0200

    Results, Übersetzungen, usw gehen

--- src/components/SurveyForm.js
+++ src/components/SurveyForm.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { useNavigate } from "react-router-dom"; // Import useNavigate
+import { useNavigate } from "react-router-dom";
 import { Survey } from "survey-react-ui";
 import "survey-core/defaultV2.min.css";
 import "./override.css";
@@ -9,12 +9,26 @@ import getSurveyData from "./data/surveyData";
 
 StylesManager.applyTheme("defaultV2");
 
+const preprocessSurveyData = (data) => {
+  data.pages.forEach((page) => {
+    const imageElementIndex = page.elements.findIndex(
+      (el) => el.type === "image"
+    );
+    if (imageElementIndex > 0) {
+      const [imageElement] = page.elements.splice(imageElementIndex, 1);
+      page.elements.unshift(imageElement);
+    }
+  });
+  return data;
+};
+
 const SurveyForm = ({ onComplete, locale }) => {
   const [surveyData, setSurveyData] = useState(null);
-  const navigate = useNavigate(); // Initialize useNavigate
+  const navigate = useNavigate();
 
   useEffect(() => {
-    setSurveyData(getSurveyData(locale));
+    const data = getSurveyData(locale);
+    setSurveyData(preprocessSurveyData(data));
   }, [locale]);
 
   if (!surveyData) {
@@ -28,7 +42,7 @@ const SurveyForm = ({ onComplete, locale }) => {
 
     try {
       await axios.post("http://lnx-007.khm.at/api/survey/", survey.data);
-      navigate("/results"); // Redirect to results page
+      navigate("/results");
     } catch (error) {
       console.error("There was an error saving the survey responses:", error);
     }
